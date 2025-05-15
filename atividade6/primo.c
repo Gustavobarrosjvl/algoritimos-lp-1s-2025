34003a1c526c8c299e7966676ff32ce38ec8333a
gustavobarrosjvl <gustavobarros.ctto@gmail.com>
2025-05-15 12:13:03 -0300

    primos

---
+++ atividade6/primo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[]){
+    int numero;
+
+    printf("\n VERIFICADOR DE NUMERO PRIMO\n");
+    printf("Digite o numero que deseja verificar: ");
+    scanf("%d", &numero);
+
+    return 0;
+}
