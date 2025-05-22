13d3cad4bcd5465c59419fc9192412bd0602cb02
gustavobarrosjvl <gustavobarros.ctto@gmail.com>
2025-05-22 12:22:02 -0300

    nota

---
+++ funcao/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define TAM_NOME 64
+
+typedef struct Aluno{
+    char nome[TAM_NOME];
+    float nota;
+} Aluno;
+
+void imprimir_aluno(struct Aluno a){
+    printf("\nNome: %sNota: %.2f\n", a.nome, a.nota);
+}
+
+int main(int argc, char* argv[]){
+
+    int n = 5;
+    Aluno alunos[n];
+
+    printf("Digite o nome do aluno 0: ");
+    fgets(alunos[0].nome, TAM_NOME, stdin);
+    printf("Digite a nota do aluno 0: ");
+    scanf("%f", &alunos[0].nota);
+
+    imprimir_aluno(alunos[0]);
+
+    return 0;
+}
